b7bcee4c0afb8b712c5be9234dcfe728c6155bad
Volodymyr Mandziuk <vmandziuk@gmail.com>
2026-01-27 13:11:38 +0200

    FireStore Database setting

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,16 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAu-jyt5BPHAFArfg2v06SaYGACaLF8C0o",
-  authDomain: "url-shortener-62866.firebaseapp.com",
-  projectId: "url-shortener-62866",
-  storageBucket: "url-shortener-62866.firebasestorage.app",
-  messagingSenderId: "717379351308",
-  appId: "1:717379351308:web:e412c08e0de9a960118f29",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore
 export const db = getFirestore(app);
